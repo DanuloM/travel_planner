735f26f51e0ab0bb038a16dc8ceff1d412a3c697
DanuloM <danulo1337@gmail.com>
2026-03-18 16:37:43 +0200

    Add basic pagination

--- core/settings.py
+++ core/settings.py
@@ -121,4 +121,6 @@ STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
